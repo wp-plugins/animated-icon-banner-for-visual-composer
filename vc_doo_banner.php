ce74198f1550c88ccdf4f2bcdae2bcaa7c813c4b
diego.benna <diego.benna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-09 20:00:44 +0000

    New version 1.2 add image doo banner
    git-svn-id: https://plugins.svn.wordpress.org/animated-icon-banner-for-visual-composer/trunk@1177848 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vc_doo_banner.php
+++ vc_doo_banner.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Animated Doo Banners
 Description: Advance Banner for Visual Composer
-Version: 1.1
+Version: 1.2
 Author: Diego Benna
 Author URI: http://www.diegobenna.it
 License: GPL2
